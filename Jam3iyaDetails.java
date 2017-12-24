396f2b9747a4ae5a3f48dc49225a0afdfda6c3f1
sjahaf <salj4215@google.com>
2017-12-24 01:42:03 -0500

    Adding buttons in Home screen and creating details activity

---
+++ Jam3iyaDetails.java
@@ -0,0 +1,13 @@
+package com.cybersynergysoftworks.jam3iyahcalcandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Jam3iyaDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jam3iya_details);
+    }
+}
