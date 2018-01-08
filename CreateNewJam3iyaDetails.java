91f338d94d568b26282e99be9f13b4dfc9aad4ba
Salleh Jahaf <salj4215@gmail.com>
2018-01-08 00:09:06 -0500

    added add jam3iya button and new jam3iya details activity

---
+++ CreateNewJam3iyaDetails.java
@@ -0,0 +1,13 @@
+package com.cybersynergysoftworks.jam3iyahcalcandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateNewJam3iyaDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_jam3iya_details);
+    }
+}
